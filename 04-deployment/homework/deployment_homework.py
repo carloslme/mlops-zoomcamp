e3544493b5afa0c6db1510e0ad0035518e62f80a
carloslme <32799711+carloslme@users.noreply.github.com>
2023-06-26 15:22:51 -0600

    remove comments

--- 04-deployment/homework/deployment_homework.py
+++ 04-deployment/homework/deployment_homework.py
@@ -1,15 +1,12 @@
-# %% [markdown]
-
-# %%
-import pickle
-import pandas as pd
 import sys
 
-# %%
+import numpy as np
+import pandas as pd
+import pickle
+
 with open('model.bin', 'rb') as f_in:
     dv, model = pickle.load(f_in)
 
-# %%
 categorical = ['PULocationID', 'DOLocationID']
 
 def read_data(filename):
@@ -29,13 +26,10 @@ month = int(sys.argv[2])
 
 df = read_data(f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-0{month}.parquet')
 
-# %%
 dicts = df[categorical].to_dict(orient='records')
 X_val = dv.transform(dicts)
 y_pred = model.predict(X_val)
 
-# %%
-import numpy as np
 # Calculate the standard deviation
 std_dev = np.std(y_pred)
 
@@ -49,13 +43,10 @@ print("Mean:", mean)
 
 df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
 
-# %%
 df['predictions'] = y_pred
 
-# %%
 df_result = df[['ride_id','predictions']].copy()
 
-# %%
 output_file = 'data/df_result.parquet'
 
 df_result.to_parquet(
